18fecc87fce1ed5541ef77a47ccdc494fc1b8d6d
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-05-25 12:30:52 +0530

    added corp signup api and google drive storage

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from gdstorage.storage import GoogleDriveStorage
+
+# Define Google Drive Storage
+gd_storage = GoogleDriveStorage()
 
 # Create your models here.
-class User(models.Model):
+class CorpUser(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    website = models.URLField(max_length=100)
     email = models.EmailField(max_length=100)
+    contact = models.CharField(max_length=100, default='')
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pincode = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    verification_doc = models.FileField(upload_to='corp', storage=gd_storage, default='')
 
     def __str__(self):
         return self.id
