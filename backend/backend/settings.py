33b97b409395a1730778356e7e599d00b1361371
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-05-27 16:24:31 +0530

    heroku commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -145,7 +145,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
+     'http://localhost:3000',
+     'https://dataplace.netlify.app/'
 ]
 
 
