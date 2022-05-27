4bef7810660d1594e24209f0c0ddcbd57f486760
Ayush Malik <ayushmalik779@gmail.com>
2022-05-27 10:40:20 +0530

    Added College User model and API

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from .views import CorpUserView
+from .views import ColUserView
 
 urlpatterns = [
     path('corp_user', CorpUserView.as_view()),
+    path('coll_user' , ColUserView.as_view()),
 ]
