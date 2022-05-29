e6c38462c43259ab7fcbdb3cee28d7f38ecb1284
Aaditya Singhal <aadityasinghal1978@gmail.com>
2022-05-29 20:50:50 +0530

    updated email send variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,4 +153,11 @@ CORS_ORIGIN_WHITELIST = [
 # Google Drive Storage Settings
 GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = None
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
 django_heroku.settings(locals())
