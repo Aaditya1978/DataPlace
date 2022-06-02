937397e896c7d0673f65775d3b32cb44f927fec5
Ayush Malik <ayushmalik779@gmail.com>
2022-06-02 12:07:12 +0530

    Added forgot password API's

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,10 +1,11 @@
+from msilib.schema import ReserveCost
 from django.urls import path, include
-from .views import CorpUserView
-from .views import ColUserView
-from .views import VerifyToken
+from .views import CorpUserView , ColUserView, ResetPassword , VerifyToken , SendPasswordResetMail , ResetPassword
 
 urlpatterns = [
     path('corp_user', CorpUserView.as_view()),
     path('coll_user' , ColUserView.as_view()),
-    path('verify_token' , VerifyToken.as_view())
-]
+    path('verify_token' , VerifyToken.as_view()),
+    path('send_password_mail' , SendPasswordResetMail),
+    path('change_password' , ResetPassword),
+] 
