16f221a15c8a546f9a397efca9a3c2cc320e12d7
Aaditya Singhal <aadityasinghal1978@gmail.com>
2023-03-13 21:51:25 +0530

    updated college model

--- backend/models/db.js
+++ backend/models/db.js
@@ -9,7 +9,6 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   logging: false,
-  operatorsAliases: false,
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
